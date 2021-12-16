49a492091120ee953272eecfeb8c248b83d5753b
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-12-16 14:30:01 +0200

    инициализация календаря task-02

---
+++ src/js/02-timer.js
@@ -0,0 +1,7 @@
+// Описан в документации
+import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
+import 'flatpickr/dist/flatpickr.min.css';
+
+// иницифлизация календаря====
+flatpickr("#datetime-picker", {});
