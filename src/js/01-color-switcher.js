3ed90cdeb3081d9067638dcd58c6eed5d9ad3ab8
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-12-16 15:34:41 +0200

    task-01 смена цветов

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
 
+
+// =функция смены цветов раз в 1сек==
+
+const buttonColorStart = document.querySelector('[data-start]');
+const buttonColorStop = document.querySelector('[data-stop]');
+// const refs = {
+//    startBtn: document.querySelector('button[data-start]'),
+//    stopBtn: document.querySelector('button[data-stop]'),
+// };
+
+buttonColorStart.addEventListener('click', startColorClick)
+// buttonColorStop.addEventListener('click', stopColorClick)
+
+function startColorClick() {
+   setInterval(
+      function () {
+         var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+         document.body.style.backgroundColor = "#" + randomColor;
+      }, 1000);
+}
+
+
