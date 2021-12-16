0241824e08fd74122b1bb559eba03f0462cf04bf
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-12-16 22:10:10 +0200

    добавлен stop task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,19 @@
 
 
-// =функция смены цветов раз в 1сек==
 
-const buttonColorStart = document.querySelector('[data-start]');
-const buttonColorStop = document.querySelector('[data-stop]');
-// const refs = {
-//    startBtn: document.querySelector('button[data-start]'),
-//    stopBtn: document.querySelector('button[data-stop]'),
-// };
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+let timerId = null;
 
-buttonColorStart.addEventListener('click', startColorClick)
-// buttonColorStop.addEventListener('click', stopColorClick)
-
-function startColorClick() {
-   setInterval(
-      function () {
-         var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-         document.body.style.backgroundColor = "#" + randomColor;
-      }, 1000);
-}
+startBtn.addEventListener("click", () => {
+   timerId = setInterval(() => {
+      var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      document.body.style.backgroundColor = "#" + randomColor;
+   }, 1000);
+});
 
 
+stopBtn.addEventListener("click", () => {
+   clearInterval(timerId);
+   console.log(`Interval with id ${timerId} has stopped!`);
+});
