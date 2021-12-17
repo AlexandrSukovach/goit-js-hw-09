d9146ac04fd819316cc707ed8315fb3adb07ebb3
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-12-17 13:12:49 +0200

    добавление disablet в task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,38 @@
 
-
-
 const startBtn = document.querySelector("[data-start]");
 const stopBtn = document.querySelector("[data-stop]");
+const bodyColor = document.querySelector("body");
+
+startBtn.classList.add('btnStart');
+stopBtn.classList.add('btnStop');
+bodyColor.classList.add('styleBody');
+
 let timerId = null;
+let startStop = true;
+// ==добавление стилей====
+bodyColor.style.display = 'flex';
+startBtn.setAttribute("style", "cursor: pointer;padding: 15px 30px;margin-left: auto;margin-top: 20em;");
+stopBtn.setAttribute("style", "cursor: pointer;padding: 15px 30px;margin-left: 30px;margin-right: auto;margin-top: 20em;");
 
 startBtn.addEventListener("click", () => {
-   timerId = setInterval(() => {
-      var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-      document.body.style.backgroundColor = "#" + randomColor;
-   }, 1000);
-});
+   if (startStop === true) {
+      timerId = setInterval(() => {
+         var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+         document.body.style.backgroundColor = "#" + randomColor;
+      }, 1000);
+      startStop = false;
+      // ===добавление ликвидация disabled====
+      startBtn.setAttribute('disabled', 'disabled');
+      stopBtn.removeAttribute("disabled");
+   };
 
+});
 
 stopBtn.addEventListener("click", () => {
    clearInterval(timerId);
-   console.log(`Interval with id ${timerId} has stopped!`);
+   startStop = true;
+   // ===добавление ликвидация disabled====
+   stopBtn.setAttribute('disabled', 'disabled');
+   startBtn.removeAttribute("disabled");
+   // console.log(`Interval with id ${timerId} has stopped!`);
 });
